001bae2e076bf378a2eb0e13f1ed0b477c1495c6
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-08 23:24:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash[dennis_ritchie][:language}
+puts programmer_hash[dennis_ritchie][:language}
 end
 
 def adding_matz
@@ -82,7 +82,7 @@ def adding_matz
         }
      }
 
-programmer_hash << ":yukihiro_matsumoto => {
+puts programmer_hash << ":yukihiro_matsumoto => {
   :known_for => "Ruby",
     :languages => ["LISP", "C"]
 }]"
@@ -106,7 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-  programmer_hash   
+
+puts programmer_hash[:alan_kay][:known_for] => alans_new_info   
      
 end
 
@@ -126,5 +127,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+puts programmer_hash[:dennis_ritchie][:languages] = "Assembly"
 end
