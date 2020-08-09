4e9bd20d5fb4b94f5a2a26df10ba7d7ca57c9963
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-09 00:09:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,5 +127,4 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
-end
+programmer_hash[:dennis_ritchie][:languages] ["C"].to_s endd
